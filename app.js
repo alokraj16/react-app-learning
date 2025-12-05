bbe00bde7942ad3cdb9680b9b111cb8c30069f63
alokraj16 <alokraj1611golu@gmail.com>
2025-12-05 16:32:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello in world  React"
+// );
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Hello h1 tag")
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
