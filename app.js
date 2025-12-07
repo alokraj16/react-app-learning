dd7ac367e3f386b5f111f8d762528fbef6735d87
alokraj16 <alokraj1611golu@gmail.com>
2025-12-07 23:27:15 +0530

    update logic

--- app.js
+++ app.js
@@ -6,14 +6,52 @@
 // console.log(heading);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Hello h1 tag")
-  )
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+// const jsxHeading = <h1 id="heading">Hello jsxHaeding</h1>;
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "Hello h1 tag")
+//   )
+// );
+
+const Age = () => {
+  const ages = 20;
+  return (
+    <div>
+      {ages >= 18 ? (
+        <h1 className="heading">My age is:{ages}</h1>
+      ) : (
+        <h1 className="heading">My age is not valid:{ages}</h1>
+      )}
+    </div>
+  );
+};
+
+function Header() {
+  return <h1 className="heading">Welcome to My Website</h1>;
+}
+const Fotter = () => {
+  return <h1 className="heading">This is fotter</h1>;
+};
+const Alok = () => {
+  return <h1 className="heading">My Name is ALok</h1>;
+};
+export { Alok };
+export { Header };
+export { Fotter };
+export default Age;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(
+  <div>
+    <Header />
+    <Age />
+    <Alok />
+    <Fotter />
+  </div>
+);
